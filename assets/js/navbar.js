8ed21e0db38af7c7fb91c3db11bab5aeed41a271
Calyx James <183707182+lyxxqtt@users.noreply.github.com>
2025-09-25 22:35:25 +0800

    Add files via upload

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,15 +1,36 @@
-const navLinks = document.querySelector('.nav-links');
-const hamburger = document.querySelector('.hamburger');
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+  const navItems = document.querySelectorAll('.nav-links a');
 
-hamburger.addEventListener('click', () => {
-  if (navLinks.classList.contains('open')) {
-    navLinks.classList.remove('open');
-    navLinks.classList.add('closing');
-    setTimeout(() => {
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    if (navLinks.classList.contains('open')) {
+      navLinks.classList.remove('open');
+      navLinks.classList.add('closing');
+    } else {
       navLinks.classList.remove('closing');
-    }, 400); 
-  } else {
-    navLinks.classList.add('open');
-  }
-  hamburger.classList.toggle('open');
-});
+      navLinks.classList.add('open');
+    }
+  });
+
+  // Close nav when clicking a link
+  navItems.forEach(link => {
+    link.addEventListener('click', () => {
+      if (window.innerWidth <= 767) {
+        navLinks.classList.remove('open');
+        navLinks.classList.add('closing');
+        hamburger.classList.remove('open');
+
+        setTimeout(() => {
+          navLinks.classList.remove('closing');
+        }, 400); 
+      }
+    });
+  });
+
+  // ANTI SCROLL once na nakaopen
+if (navLinks.classList.contains('open')) {
+  document.body.style.overflow = 'hidden';
+} else {
+  document.body.style.overflow = '';
+}
